9997699283233a88eca8c83c65ec88dbfbbd0822
John Evans <lgastako@gmail.com>
2013-04-22 04:22:52 -0700

    txs

--- drapes.py
+++ drapes.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+
 import db as db_
 
 
@@ -11,27 +13,39 @@ class Draper(object):
 
     def __init__(self, db=None, *mixins):
         self._mixins = mixins
-        self._db = db or db_
+        self.db = db or db_
 
     def _wrap(self, row):
         # TODO: Create class return it here etc
         return row
 
     def item(self, *args, **kwargs):
-        return self._wrap(self._db.item(*args, **kwargs))
+        return self._wrap(self.db.item(*args, **kwargs))
 
     def items(self, *args, **kwargs):
-        return map(self._wrap, self._db.items(*args, **kwargs))
+        return map(self._wrap, self.db.items(*args, **kwargs))
 
     def first(self, *args, **kwargs):
-        x = self._db.first(*args, **kwargs)
+        x = self.db.first(*args, **kwargs)
         if x:
             return self._wrap(x)
 
     def count(self, *args, **kwargs):
-        return self._db.count(*args, **kwargs)
+        return self.db.count(*args, **kwargs)
+
+    def do(self, *args, **kwargs):
+        self.db.do(*args, **kwargs)
+
+    @contextmanager
+    def tx(self, *args, **kwargs):
+        with self.db.tx(*args, **kwargs) as tx:
+            yield tx
+
+    @contextmanager
+    def txc(self, *args, **kwargs):
+        with self.db.txc(*args, **kwargs) as (conn, cursor):
+            yield conn, cursor
 
 
 def drape(name, db=None, *mixins):
     return Draper(*mixins)
-
--- test_drapes.py
+++ test_drapes.py
@@ -4,7 +4,7 @@ import sqlite3
 from drapes import drape
 
 
-class TestNull:
+class TestNullPassthroughs:
     
     def setup_method(self, method):
         self.Null = drape("Null") 
@@ -14,19 +14,34 @@ class TestNull:
         db.do("CREATE TABLE tests (name TEXT)")
         db.do("INSERT INTO tests (name) VALUES ('foo')")
 
-    def test_item_passthrough(self):
+    def test_item(self):
         self.sql = "SELECT COUNT(*) AS n FROM tests"
         assert self.Null.item(self.sql).n == db.item(self.sql).n == 1
 
-    def test_items_passthrough(self):
+    def test_items(self):
         self.sql = "SELECT name FROM tests"
         null_items = self.Null.items(self.sql)
         db_items = db.items(self.sql)
         nn = lambda xs: map(lambda x: x.name, xs)
         assert nn(null_items) == nn(db_items) == ["foo"]
 
-    def test_first_passthrough(self):
+    def test_first(self):
         assert self.Null.first(self.sql).n == db.first(self.sql).n == 1
 
-    def test_count_passthrough(self):
+    def test_count(self):
         assert self.Null.count("tests") == db.count("tests") == 1
+
+    def test_do(self):
+        self.Null.do("INSERT INTO tests (name) VALUES ('bar')")
+        assert self.Null.count("tests") == 2
+
+    def test_tx(self):
+        with self.Null.tx() as tx:
+            assert tx.count("tests") == 1
+            
+    def test_txc(self):
+        with self.Null.txc() as (conn, cursor):
+            cursor.execute("SELECT COUNT(*) AS n FROM tests")
+            assert cursor.fetchone().n == 1
+            
+        
